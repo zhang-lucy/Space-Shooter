17c64a9fee825922e8625e5d4fd20b500083512b
Lucy Zhang <zhangly@mit.edu>
2018-02-22 04:15:34 -0500

    upload files

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+	private Rigidbody rb;
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward*speed;
+	}
+
+	private void Update()
+	{
+		if (rb.gameObject.transform.position.z <= -10||rb.gameObject.transform.position.z >= 20)
+		{
+			Destroy(rb.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (Input.GetButton("Jump") && Time.time > nextFire)
+		{
+			GameObject clone;
+			clone = Instantiate(shot) as GameObject;
+			nextFire = Time.time + fireRate;
+			clone.transform.position = shotSpawn.transform.position;
+			GetComponent<AudioSource>().Play();		}
+		
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = speed*movement;
+
+		rb.position = new Vector3
+		(
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
+		
+		rb.rotation = Quaternion.Euler(0.0f,0.0f,rb.velocity.x*(-tilt));
+	}
+	
+}
